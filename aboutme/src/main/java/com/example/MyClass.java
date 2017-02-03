6343664ac3e0f9345d2974ae199a8fb18cf25b67
George Apostolopoulos <LostSouls@Georges-iMac.local>
2017-02-02 23:16:14 -0600

    Initial commit

---
+++ aboutme/src/main/java/com/example/MyClass.java
@@ -0,0 +1,19 @@
+package com.example;
+
+public class MyClass {
+    public static void main(String[] args)
+    {
+        String myName = "George Apostolopoulos";
+        String major = "Computer Science";
+        double gpa = 3.75;
+        char grade = 'A';
+        int started = 2014;
+        System.out.println("Hello, my name is " + myName);
+        System.out.println("I am majoring in " + major);
+        System.out.println("My goal this semester is to get a " + gpa +
+                " or higher for my GPA");
+        System.out.println("The grade I hope to get in this class is an " + grade);
+        System.out.println("I first started at MCC in " + started);
+    }
+
+}
---
+++ initializevariables/src/main/java/com/example/MyClass.java
@@ -0,0 +1,26 @@
+package com.example;
+
+public class MyClass {
+    public static void main(String[] args){
+        byte a = 100;
+        short b = 0;
+        int number = 4000;
+        long c = 3L;
+        float d = 4.9f;
+        double e = 4.4567890d;
+        Boolean variable = true;
+        char me = 'a';
+        String myName = "George Apostolopoulos";
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(number);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(variable);
+        System.out.println(me);
+        System.out.println(myName);
+
+    }
+}
